3611bcd3b1ab2d9fdeeabaed95509fb919599e6f
xamilll <90074553+xamilll@users.noreply.github.com>
2021-12-28 22:27:49 +0100

    Delete configure_tc.sh

--- scripts/configure_tc.sh
+++
@@ -1,8 +0,0 @@
-sif=lan0
-dif=dummy0
-# ingress
-tc qdisc add dev "$sif" ingress
-tc filter add dev "$sif" parent ffff: protocol all u32 match u8 0 0 action mirred egress mirror dev "$dif"
-# egress
-tc qdisc add dev "$sif" handle 1: root prio
-tc filter add dev "$sif" parent 1: protocol all u32 match u8 0 0 action mirred egress mirror dev "$dif"
